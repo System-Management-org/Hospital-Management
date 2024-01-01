939f78519eeeb9f25a59afe076dd93b6bab7cc59
KAsare1 <asarek977@gmail.com>
2024-01-01 13:17:28 +0000

    deleting authentication app

--- HMS/HMS/settings.py
+++ HMS/HMS/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'authentication',
     'corsheaders',
     'rest_framework_simplejwt.token_blacklist'
 ]
