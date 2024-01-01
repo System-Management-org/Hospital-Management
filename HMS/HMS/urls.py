212168fbadd6264aabb8ee1d771d65ce7eb505eb
KAsare1 <asarek977@gmail.com>
2024-01-01 13:40:26 +0000

    putting back admin url

--- HMS/HMS/urls.py
+++ HMS/HMS/urls.py
@@ -37,6 +37,7 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     # path(r'^', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
