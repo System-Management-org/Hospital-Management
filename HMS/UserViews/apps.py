3817f4b99c1f698262718b5786765397cf1d5e46
Kofi Asare-Amankwah <111601374+KAsare1@users.noreply.github.com>
2024-01-31 05:52:48 +0000

    Delete HMS directory

--- HMS/UserViews/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserviewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'UserViews'
--- HMS/UserViews/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-
-urlpatterns = [
-    path('patient/', views.PatientList.as_view(), name="patient_list"),
-    path('patient/<str:patient_id>/', views.PatientDetail.as_view(), name="patient_detail"),
-]
